184268a38b0c77fc4d7272efe2612a8666ffa0f1
Rajesh Singh <rajeshsh565@gmail.com>
2025-08-11 09:38:20 +0530

    added useEffect import for register page

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import CustomAuthInput from "../Components/CustomAuthInput";
 import customFetch from "../utils/customFetch";
